a4d074477b518a9a76f8a908bba48f645d106cb9
backslashxx <118538522+backslashxx@users.noreply.github.com>
2024-02-27 21:20:38 +0800

    sched/bore: enable sched_burst_smoothness_short by default
    results in way better jitter test results
    
    Signed-off-by: Mr-Morat <mtarom124@gmail.com>
    Signed-off-by: manipvlator <mr.morat14@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -175,7 +175,7 @@ static inline struct task_struct *task_of(struct sched_entity *se)
 #ifdef CONFIG_SCHED_BORE
 uint __read_mostly sched_bore                   = 1;
 uint __read_mostly sched_burst_smoothness_long  = 1;
-uint __read_mostly sched_burst_smoothness_short = 0;
+uint __read_mostly sched_burst_smoothness_short = 1;
 uint __read_mostly sched_burst_fork_atavistic   = 2;
 uint __read_mostly sched_burst_penalty_offset   = 22;
 uint __read_mostly sched_burst_penalty_scale    = 1280;
