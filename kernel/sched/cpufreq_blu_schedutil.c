bbff069852126a32ab9b06db6fc94f78d63e3b72
Thiago <thigo6617@gmail.com>
2026-06-17 12:18:02 +0000

    fixup blu

--- kernel/sched/cpufreq_blu_schedutil.c
+++ kernel/sched/cpufreq_blu_schedutil.c
@@ -16,6 +16,7 @@
 
 #include <linux/cpufreq.h>
 #include <linux/kthread.h>
+#include <uapi/linux/sched/types.h>
 #include <linux/slab.h>
 
 #include "sched.h"
@@ -207,6 +208,7 @@ static void sugov_get_util(unsigned long *util, unsigned long *max, u64 time)
 	struct rq *rq = cpu_rq(cpu);
 	unsigned long max_cap, rt;
 	s64 delta;
+	struct sched_walt_cpu_load walt_load = {0};
 
 	max_cap = capacity_orig_of(cpu);
 
@@ -217,7 +219,6 @@ static void sugov_get_util(unsigned long *util, unsigned long *max, u64 time)
 	rt = div64_u64(rq->rt_avg, sched_avg_period() + delta);
 	rt = (rt * max_cap) >> SCHED_CAPACITY_SHIFT;
 
-	struct sched_walt_cpu_load walt_load = {0};
 	*util = boosted_cpu_util(cpu, &walt_load);
 	if (likely(use_pelt()))
 		*util = min((*util + rt), max_cap);
